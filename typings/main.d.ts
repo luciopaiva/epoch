817cb6bb5674ccf2834b2b1d98097b9999583f0a
Lucio Paiva <luciopaiva@gmail.com>
2016-03-26 22:27:28 -0300

    Initial release

---
+++ typings/main.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="main/ambient/d3/index.d.ts" />
+/// <reference path="main/ambient/moment-node/index.d.ts" />
+/// <reference path="main/ambient/moment/index.d.ts" />
+/// <reference path="main/ambient/require/index.d.ts" />
